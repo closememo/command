a8516eaa4a37536aef1948481191c56ee1787503
bitgadak <bitgadak@gmail.com>
2021-12-13 01:40:45 +0900

    gradle, plugin 버전 업 및 이로 인해 발생한 오류 수정
    - 빈 생성 이후 Autowired 로 처리하던 부분을 클래스로 분리하여 PostConstruct 를 이용하여 처리

--- src/main/java/com/closememo/command/config/messaging/integration/IntegrationConfig.java
+++ src/main/java/com/closememo/command/config/messaging/integration/IntegrationConfig.java
@@ -1,11 +1,11 @@
 package com.closememo.command.config.messaging.integration;
 
+import com.closememo.command.application.Command;
 import com.closememo.command.domain.DomainEvent;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import com.closememo.command.application.Command;
+import javax.annotation.PostConstruct;
 import lombok.extern.slf4j.Slf4j;
 import org.reflections.Reflections;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -20,6 +20,7 @@ import org.springframework.messaging.Message;
 import org.springframework.messaging.MessageHandler;
 import org.springframework.messaging.support.MessageBuilder;
 import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
+import org.springframework.stereotype.Component;
 import org.springframework.web.context.support.GenericWebApplicationContext;
 
 @Slf4j
@@ -56,33 +57,53 @@ public class IntegrationConfig {
     };
   }
 
-  @Bean
-  public CommandClassResolver commandClassResolver() {
-    Reflections reflections = new Reflections("com.closememo.command.application");
-    return new CommandClassResolver(reflections.getSubTypesOf(Command.class));
-  }
+  @Component
+  public static class Initializer {
 
-  @Autowired
-  private void registerCommandChannels(GenericWebApplicationContext context,
-      @Qualifier("messageTaskExecutor") ThreadPoolTaskExecutor messageTaskExecutor) {
-    commandClassResolver().getNames()
-        .forEach(name -> context.registerBean(name, ExecutorChannel.class,
-            () -> new ExecutorChannel(messageTaskExecutor)));
-  }
+    private final GenericWebApplicationContext context;
+    private final ThreadPoolTaskExecutor messageTaskExecutor;
+    private final MessageHandler broadcastDomainEventHandler;
 
-  @Autowired
-  private void registerDomainEventChannels(GenericWebApplicationContext context,
-      @Qualifier("messageTaskExecutor") ThreadPoolTaskExecutor messageTaskExecutor,
-      @Qualifier("broadcastDomainEventHandler") MessageHandler broadcastDomainEventHandler) {
-    Reflections reflections = new Reflections("com.closememo.command.domain");
-    reflections.getSubTypesOf(DomainEvent.class).forEach(aClass -> {
-      String eventChannelName = aClass.getSimpleName();
+    public Initializer(
+        GenericWebApplicationContext context,
+        @Qualifier("messageTaskExecutor") ThreadPoolTaskExecutor messageTaskExecutor,
+        @Qualifier("broadcastDomainEventHandler") MessageHandler broadcastDomainEventHandler) {
+      this.context = context;
+      this.messageTaskExecutor = messageTaskExecutor;
+      this.broadcastDomainEventHandler = broadcastDomainEventHandler;
+    }
 
-      context.registerBean(eventChannelName, PublishSubscribeChannel.class,
-          () -> new PublishSubscribeChannel(messageTaskExecutor));
-      PublishSubscribeChannel eventChannel = context.getBean(eventChannelName, PublishSubscribeChannel.class);
-      context.registerBean("Broadcast-" + eventChannelName, EventDrivenConsumer.class,
-          eventChannel, broadcastDomainEventHandler);
-    });
+    @PostConstruct
+    public void init() {
+      registerCommandChannels(context, messageTaskExecutor);
+      registerDomainEventChannels(context, messageTaskExecutor, broadcastDomainEventHandler);
+    }
+
+    private void registerCommandChannels(GenericWebApplicationContext context,
+        ThreadPoolTaskExecutor messageTaskExecutor) {
+
+      Reflections reflections = new Reflections("com.closememo.command.application");
+      CommandClassResolver commandClassResolver = new CommandClassResolver(
+          reflections.getSubTypesOf(Command.class));
+
+      commandClassResolver.getNames()
+          .forEach(name -> context.registerBean(name, ExecutorChannel.class,
+              () -> new ExecutorChannel(messageTaskExecutor)));
+    }
+
+    private void registerDomainEventChannels(GenericWebApplicationContext context,
+        ThreadPoolTaskExecutor messageTaskExecutor, MessageHandler broadcastDomainEventHandler) {
+      Reflections reflections = new Reflections("com.closememo.command.domain");
+      reflections.getSubTypesOf(DomainEvent.class).forEach(aClass -> {
+        String eventChannelName = aClass.getSimpleName();
+
+        context.registerBean(eventChannelName, PublishSubscribeChannel.class,
+            () -> new PublishSubscribeChannel(messageTaskExecutor));
+        PublishSubscribeChannel eventChannel = context
+            .getBean(eventChannelName, PublishSubscribeChannel.class);
+        context.registerBean("Broadcast-" + eventChannelName, EventDrivenConsumer.class,
+            eventChannel, broadcastDomainEventHandler);
+      });
+    }
   }
 }
