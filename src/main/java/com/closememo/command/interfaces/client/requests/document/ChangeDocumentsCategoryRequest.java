03b4b960055b0420ab52731f1f16aec0ec5c165d
bitgadak <bitgadak@gmail.com>
2021-12-29 14:23:44 +0900

    String List 에 대한 @NotBlank, @NotNull 처리

--- src/main/java/com/closememo/command/interfaces/client/requests/document/ChangeDocumentsCategoryRequest.java
+++ src/main/java/com/closememo/command/interfaces/client/requests/document/ChangeDocumentsCategoryRequest.java
@@ -2,13 +2,14 @@ package com.closememo.command.interfaces.client.requests.document;
 
 import java.util.List;
 import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
 import lombok.Getter;
 
 @Getter
 public class ChangeDocumentsCategoryRequest {
 
-  @NotBlank
-  private List<String> documentIds;
+  @NotNull
+  private List<@NotBlank String> documentIds;
   @NotBlank
   private String categoryId;
 }
